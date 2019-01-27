aff0bbb6474aa8884c6f218ef4dfddb20a8862ec
Dave Sag <davesag@gmail.com>
2019-01-27 17:26:06 +0530

    n/a added a delayed adder test

---
+++ test/integration/delegateAsyncTask.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai')
+const makeWorker = require('../../src/makeWorker')
+const makeDelegator = require('../../src/makeDelegator')
+
+describe('delegate an asynchronous task', () => {
+  const task = (a, b) =>
+    new Promise(resolve => setTimeout(() => resolve(a + b), 10))
+
+  const worker = makeWorker({
+    name: 'adder',
+    task
+  })
+
+  const delegator = makeDelegator({ exchange: 'test' })
+
+  let result
+
+  before(async () => {
+    await worker.start()
+    await delegator.start()
+    result = await delegator.invoke('adder', 10, 15)
+  })
+
+  after(async () => {
+    await worker.stop()
+    await delegator.stop()
+  })
+
+  it('returned the right result', () => {
+    expect(result).to.equal(25)
+  })
+})
