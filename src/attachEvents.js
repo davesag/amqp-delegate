758d3b7069d0e03913f10d0333a21e747266c7c1
Dave Sag <davesag@gmail.com>
2019-01-28 09:16:44 +0530

    #1 added docs and notes

--- src/attachEvents.js
+++ src/attachEvents.js
@@ -1,3 +1,9 @@
+/**
+ *  Attaches onError and onClose event handlers
+ *  to the supplied connection.
+ *  @param connection — an amqp connection
+ *  @param handers — onError and onClose event handler functions.
+ */
 const attachEvents = (connection, { onError, onClose }) => {
   const attach = (event, handler) => {
     if (typeof handler === 'function') connection.on(event, handler)
